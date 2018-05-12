17f76e57c84c5719fc76a4160ccbfbbf564125b0
AmyHSW <huang.shuw@husky.neu.edu>
2018-05-12 19:11:50 +0800

    finish basic requirements in hw1

--- app/src/main/java/edu/neu/madcourse/shuwanhuang/numad18s_shuwanhuang/DisplayMessageActivity.java
+++ app/src/main/java/edu/neu/madcourse/shuwanhuang/numad18s_shuwanhuang/DisplayMessageActivity.java
@@ -1,6 +1,6 @@
 package edu.neu.madcourse.shuwanhuang.numad18s_shuwanhuang;
 
-import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -13,11 +13,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
 
         // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+/*        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);*/
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        TextView textView = findViewById(R.id.phoneId);
+        // textView.setText(message);
+
+        String phoneId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        String text = "Phone ID: " + phoneId;
+        textView.setText(text);
     }
 }
--- app/src/main/java/edu/neu/madcourse/shuwanhuang/numad18s_shuwanhuang/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/shuwanhuang/numad18s_shuwanhuang/MainActivity.java
@@ -1,31 +1,58 @@
 package edu.neu.madcourse.shuwanhuang.numad18s_shuwanhuang;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE
-            = "edu.neu.madcourse.shuwanhuang.numad18s_shuwanhuang.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayVersionInfo();
+    }
+
+    private void displayVersionInfo() {
+        String versionName = "";
+        int versionCode = 1;
+        try {
+            versionName = getApplicationContext()
+                            .getPackageManager().getPackageInfo(getApplicationContext()
+                            .getPackageName(), 0).versionName;
+            versionCode = getApplicationContext()
+                    .getPackageManager().getPackageInfo(getApplicationContext()
+                            .getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView1 = findViewById(R.id.textView1);
+        String text1 = "Version Code: " + versionCode;
+        textView1.setText(text1);
+
+        TextView textView2 = findViewById(R.id.textView2);
+        String text2 = "Version Name: " + versionName;
+        textView2.setText(text2);
     }
 
     /**
-     * Called when the user taps the Send button.
-     * @param view
+     * Called when the user taps the About button.
+     * @param view the View object that was clicked
      */
-    public void sendMessage(View view) {
+    public void onClickAbout(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    /**
+     * Called when the user taps the Generate Error button.
+     * @param view the View object that was clicked
+     */
+    public void onClickGenerateError(View view) {
+        throw new RuntimeException("This is a crash");
     }
 }
